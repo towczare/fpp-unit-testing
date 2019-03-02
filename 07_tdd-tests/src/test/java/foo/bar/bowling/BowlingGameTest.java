8b935b06745bd8c5ccf30d42aad84c8292e4b9e7
tomic <violeticus@gmail.com>
2019-03-02 17:08:47 +0100

    - game creation + test

--- 07_tdd-tests/src/test/java/foo/bar/bowling/BowlingGameTest.java
+++ 07_tdd-tests/src/test/java/foo/bar/bowling/BowlingGameTest.java
@@ -1,4 +1,11 @@
 package foo.bar.bowling;
 
+import org.junit.jupiter.api.Test;
+
 public class BowlingGameTest {
+
+    @Test
+    public void gameCreationTest() {
+        BowlingGame game = new BowlingGame();
+    }
 }
