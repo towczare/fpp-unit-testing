84dd0e94b80648c70654d9a2dc60111415aced17
tomic <violeticus@gmail.com>
2019-03-02 18:35:42 +0100

    - passing strike test

--- 07_tdd-tests/src/main/java/foo/bar/bowling/BowlingGame.java
+++ 07_tdd-tests/src/main/java/foo/bar/bowling/BowlingGame.java
@@ -12,14 +12,20 @@ public class BowlingGame {
         int score = 0;
         int frameIndex = 0;
         for (int frame = 0; frame < 10; frame++) {
-            if (isSpare(frame)) {
-                score += 10 + rolls[frameIndex + 2];
-                frameIndex += 2;
-            } else {
-                score += rolls[frameIndex] + rolls[frameIndex + 1];
-                frameIndex += 2;
+            if(rolls[frameIndex] == 10) {
+                score = score
+                        + 10
+                        + rolls[frameIndex + 1]
+                        + rolls[frameIndex + 2];
+                frameIndex++;
+            }else if (isSpare(frame)) {
+                    score += 10 + rolls[frameIndex + 2];
+                    frameIndex += 2;
+                } else {
+                    score += rolls[frameIndex] + rolls[frameIndex + 1];
+                    frameIndex += 2;
+                }
             }
-        }
         return score;
     }
 
