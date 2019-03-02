2e10526dbc604de60b2a14b87dad2c08636d6a65
tomic <violeticus@gmail.com>
2019-03-02 17:39:22 +0100

    - refactoring, removing loop duplication and adding display names to tests

--- 07_tdd-tests/src/test/java/foo/bar/bowling/BowlingGameTest.java
+++ 07_tdd-tests/src/test/java/foo/bar/bowling/BowlingGameTest.java
@@ -1,25 +1,42 @@
 package foo.bar.bowling;
 
+import com.sun.org.glassfish.gmbal.Description;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class BowlingGameTest {
+
+
+class BowlingGameTest {
+
+    private BowlingGame game;
+
+    @BeforeEach
+    @Description("Starting new game for test purpose")
+    void setup() {
+        game = new BowlingGame();
+    }
 
     @Test
-    public void testAllZeroGame() {
-        BowlingGame game = new BowlingGame();
-        for (int i = 0; i < 20; i++) {
-            game.roll(0);
-        }
+    @DisplayName("When player roll 0 pins 20 times in a row, his score should be 0 points")
+    void testAllZeroGame() {
+        rollMany(20, 0);
+
         Assertions.assertEquals(0, game.score());
     }
 
     @Test
-    public void testAllOnesGame() {
-        BowlingGame game = new BowlingGame();
-        for (int i = 0; i < 20; i++) {
-            game.roll(1);
-        }
+    @DisplayName("When player roll 1 pin 20 times in a row, his score should be 20 points")
+    void testAllOnesGame() {
+        rollMany(20, 1);
+
         Assertions.assertEquals(20, game.score());
+    }
+
+    private void rollMany(int n, int pins) {
+        for (int i = 0; i < n; i++) {
+            game.roll(pins);
+        }
     }
 }
