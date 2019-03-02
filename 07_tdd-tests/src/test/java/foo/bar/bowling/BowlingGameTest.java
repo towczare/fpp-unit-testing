ec02531ce2ca43da86c55f283d8e73fc92f81690
tomic <violeticus@gmail.com>
2019-03-02 17:15:06 +0100

    - refactor better name of first test

--- 07_tdd-tests/src/test/java/foo/bar/bowling/BowlingGameTest.java
+++ 07_tdd-tests/src/test/java/foo/bar/bowling/BowlingGameTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 public class BowlingGameTest {
 
     @Test
-    public void gameCreationTest() {
+    public void testAllZeroGame() {
         BowlingGame game = new BowlingGame();
         for (int i = 0; i < 20; i++) {
             game.roll(0);
