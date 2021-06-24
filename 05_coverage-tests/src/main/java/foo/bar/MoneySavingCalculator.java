92768e9ad011c4ef45476d7259c9bec5e88e6218
Tomasz Owczarek <tomasz.owczarek@tomtom.com>
2021-06-24 14:05:35 +0200

    - first step of implementation

--- 05_coverage-tests/src/main/java/foo/bar/MoneySavingCalculator.java
+++ 05_coverage-tests/src/main/java/foo/bar/MoneySavingCalculator.java
@@ -5,12 +5,46 @@ import java.time.Period;
 
 public class MoneySavingCalculator {
 
+    private BigDecimal AVG_DAYS_IN_YEAR = BigDecimal.valueOf(365.25);
+    private BigDecimal AVG_DAYS_IN_MONTH = BigDecimal.valueOf(30.44);
+
     public BigDecimal calculateSavings(
             SavingFrequency frequency,
             BigDecimal singleAmmount,
             Period duration
     ) {
 
-        return singleAmmount;
+        BigDecimal days = periodToDays(duration);
+
+        BigDecimal howMany = BigDecimal.ZERO;
+
+        switch (frequency) {
+            case EACH_DAY:
+                howMany = days;
+                break;
+            case EACH_WEEK:
+                howMany = BigDecimal.valueOf(days.intValue() / 7);
+                break;
+            case EACH_MONTH:
+                break;
+            case EACH_SECOND_DAY:
+                break;
+            case TWICE_A_WEEK:
+                break;
+            case ONCE_A_YEAR:
+                break;
+        }
+
+
+        return singleAmmount.multiply(howMany);
+    }
+
+    private BigDecimal periodToDays(Period duration) {
+
+        BigDecimal daysInYears = BigDecimal.valueOf(duration.getYears()).multiply(AVG_DAYS_IN_YEAR);
+        BigDecimal daysInMonths = BigDecimal.valueOf(duration.getMonths()).multiply(AVG_DAYS_IN_MONTH);
+        BigDecimal days = BigDecimal.valueOf(duration.getDays());
+
+        return daysInYears.add(daysInMonths).add(days);
     }
 }
--- 05_coverage-tests/src/test/java/foo/bar/MoneySavingCalculatorTest.java
+++ 05_coverage-tests/src/test/java/foo/bar/MoneySavingCalculatorTest.java
@@ -4,10 +4,16 @@ package foo.bar;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.math.BigDecimal;
 import java.time.Duration;
 import java.time.Period;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MoneySavingCalculatorTest {
 
@@ -22,4 +28,17 @@ public class MoneySavingCalculatorTest {
                         BigDecimal.valueOf(1000), Period.ofYears(1))
         );
     }
+
+    private static Stream<Arguments> savings() {
+        return Stream.of(
+                Arguments.of(BigDecimal.valueOf(10), SavingFrequency.EACH_WEEK, Period.ofYears(1), BigDecimal.valueOf(520))
+        );
+    }
+
+    @ParameterizedTest(name = "When saving single amount {0} with following frequency {1} during given period {2} should save total {3}")
+    @MethodSource("savings")
+    void calculateSavings(BigDecimal singleAmount, SavingFrequency frequency, Period period, BigDecimal expectedSavings) {
+        Assertions.assertEquals(expectedSavings, calc.calculateSavings(frequency, singleAmount, period));
+    }
+
 }
